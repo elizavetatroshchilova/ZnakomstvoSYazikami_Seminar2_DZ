270f00b39bd624afbd6e651b628d6449efa12848
Elizabeth <qazwsx050816@gmail.com>
2023-02-06 00:53:02 +0300

    Add homework 2

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ Console.WriteLine("Введите трехзначное число");
+int.TryParse(Console.ReadLine(), out int a);
+int min = 100;
+int max = 1000;
+int b = a / 10 % 10;
+if (a > min && a < max)
+{
+    Console.WriteLine($"Вторая цифра {b} ");
+}
+else
+{
+    Console.WriteLine("Вы ввели недопустимое значение");
+}
---
+++ Zadanie13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+string s = n.ToString();
+int number = s.Length;
+if (number > 2)
+{
+    Console.WriteLine(s[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
